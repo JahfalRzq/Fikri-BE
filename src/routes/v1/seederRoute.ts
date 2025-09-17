cbcc7064bded3c8d4ff148cc9ab67e6fd40811bc
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-09-17 20:43:30 +0700

    feat : new endpoint for training seeder

--- src/routes/v1/seederRoute.ts
+++ src/routes/v1/seederRoute.ts
@@ -1,8 +1,20 @@
 import { Router } from "express";
 import { userSeeder } from "@/controller/seeder/userSeeder";
+import { trainingSeeder } from "@/controller/seeder/trainingSeeder";
+import {
+  authMiddleware,
+  onlyAdminMiddleware,
+} from "@/middleware/authMiddleware";
 
 const router = Router();
 
 router.get("/userSeeder", userSeeder);
+
+router.get("/trainingSeeder", [
+    authMiddleware,
+    onlyAdminMiddleware,
+    trainingSeeder
+]);
+
 
 export default router;
