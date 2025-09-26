4e199f0c4f8b202edc2ca77a74df240b1db16895
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-09-26 20:00:13 +0700

    refactor : add new route

--- src/routes/v1/authRoute.ts
+++ src/routes/v1/authRoute.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { login, fetch } from "@/controller/auth/authController";
+import { login, fetch, register } from "@/controller/auth/authController";
 const router = Router();
 import { authMiddleware } from "@/middleware/authMiddleware";
 
 router.post("/login", login);
 router.get("/fetch", [authMiddleware, fetch]);
+router.post("/register", register);
+
 
 export default router;
