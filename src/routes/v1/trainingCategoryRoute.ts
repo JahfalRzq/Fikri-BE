62f5bf58974ee3d1bb9d2c892ca1d8997670a389
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-09-26 20:10:14 +0700

    feat : training category rouute

---
+++ src/routes/v1/trainingCategoryRoute.ts
@@ -0,0 +1,45 @@
+import { Router } from "express";
+import { 
+    getAllCategoryTrainings,
+    getCategoryTrainingById,
+    createCategoryTraining,
+    updateCategoryTraining,
+    deleteCategoryTraining
+ } from "@/controller/admin/trainingManagement/categoryTrainingController";
+
+ import {
+  authMiddleware,
+  onlyAdminMiddleware,
+} from "@/middleware/authMiddleware";
+
+
+const router = Router();
+
+router.get("/get-all-training-category", [
+  authMiddleware,
+  onlyAdminMiddleware,
+  getAllCategoryTrainings,
+]);
+router.get("/get-training-category-by-id/:id", [
+  authMiddleware,
+  onlyAdminMiddleware,
+  getCategoryTrainingById,
+]);
+router.post("/create-training-category", [
+  authMiddleware,
+  onlyAdminMiddleware,
+  createCategoryTraining,
+]);
+router.put("/update-training-category/:id", [
+  authMiddleware,
+  onlyAdminMiddleware,
+  updateCategoryTraining,
+]);
+router.delete("/delete-training-category/:id", [
+  authMiddleware,
+  onlyAdminMiddleware,
+  deleteCategoryTraining,
+]);
+
+export default router;
+
