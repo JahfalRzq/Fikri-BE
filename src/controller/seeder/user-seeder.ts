82d30aba3e54e8e51bcd4aedd1bdc9b5d67fc877
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-10-07 05:06:06 +0700

    refactor : fix user seeder

--- src/controller/seeder/user-seeder.ts
+++ src/controller/seeder/user-seeder.ts
@@ -7,24 +7,24 @@ const userRepository = AppDataSource.getRepository(user);
 
 export const userSeeder = async (req: Request, res: Response) => {
   const userSeed = [{ userName: "Admin1", password: "Admin1!0)" }];
-  // try {
-  //   for (const data of userSeed) {
-  //     const newUser = new user();
-  //     newUser.userName = data.userName;
-  //     newUser.password = data.password;
-  //     newUser.role = UserRole.ADMIN;
-  //     newUser.hashPassword();
-  //     await userRepository.save(newUser);
-  //   }
-  //   console.info("User seeded successfully.");
-  // } catch (error) {
-  //   return res
-  //     .status(400)
-  //     .send(
-  //       errorResponse(
-  //         error instanceof Error ? error.message : "Unknown error occurred",
-  //         400,
-  //       ),
-  //     );
-  // }
+  try {
+    for (const data of userSeed) {
+      const newUser = new user();
+      newUser.userName = data.userName;
+      newUser.password = data.password;
+      newUser.role = UserRole.ADMIN;
+      newUser.hashPassword();
+      await userRepository.save(newUser);
+    }
+    console.info("User seeded successfully.");
+  } catch (error) {
+    return res
+      .status(400)
+      .send(
+        errorResponse(
+          error instanceof Error ? error.message : "Unknown error occurred",
+          400,
+        ),
+      );
+  }
 };
